17eb2f5fdfe03cd975405413e62cd7fdd8d693b0
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-19 16:48:17 +0100

    [UPD] exercise 03 complete

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,6 +10,7 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
+
 const fourthTeacher = teachers.indexOf('Phil', 0);
 
 console.log(fourthTeacher);
@@ -21,7 +22,12 @@ teachers.splice(4, 1, 'Patrick');
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+
+let popTeacher = teachers.pop();
+
+const lastTeacher = popTeacher;
+
+console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
 const firstTeacher = null;
